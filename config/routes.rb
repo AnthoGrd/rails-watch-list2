f1cae3049177cf4ef9299bd7c13757ab71ef9452
Anthony Girard <antho.girard18@gmail.com>
2023-11-16 16:59:16 +0100

    Rails_watch_list2/Antho_version

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
+  get 'bookmarks/index'
+  get 'bookmarks/new'
+  get 'bookmarks/show'
+  get 'bookmarks/create'
+  get 'bookmarks/edit'
+  get 'bookmarks/update'
+  get 'bookmarks/destroy'
+  get 'lists/index'
+  get 'lists/new'
+  get 'lists/show'
+  get 'lists/create'
+  get 'lists/edit'
+  get 'lists/update'
+  get 'lists/destroy'
+  get 'movies/index'
+  get 'movies/new'
+  get 'movies/show'
+  get 'movies/create'
+  get 'movies/edit'
+  get 'movies/update'
+  get 'movies/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
